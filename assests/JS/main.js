eefd4c271db779a9aebc7dbed4e6b6fda72309f5
shyamk <shyamsam1818@gmail.com>
2023-07-03 19:17:44 +0530

    first

---
+++ assests/JS/main.js
@@ -0,0 +1,26 @@
+let dayItem=document.querySelector("#days");
+let hoursItem=document.querySelector("#hours");
+let minItem=document.querySelector("#min");
+let secItem=document.querySelector("#sec");
+
+let countDown = () => { 
+    
+    let futureDate = new Date("01 August 2023");
+    let currentDate = new Date();
+    let myDate = futureDate - currentDate;
+
+    let days=Math.floor(myDate / 1000 / 60 / 60 / 24);
+    let hours=Math.floor(myDate / 1000 / 60 / 60) % 24;
+    let min=Math.floor(myDate / 1000 / 60) % 60;
+    let sec= Math.floor(myDate / 1000)%60;
+
+    dayItem.innerHTML=days;
+    hoursItem.innerHTML=hours;
+    minItem.innerHTML=min;
+    secItem.innerHTML=sec; 
+
+}
+
+countDown()
+
+setInterval(countDown, 1000)
